d4895b7474a6a3e4b57ae3e366946a63591d6076
Zion livingstone <zionlivingstone4@gmail.com>
2023-08-10 21:03:00 +0100

    x

--- app/components/Slider.jsx
+++ app/components/Slider.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { useScroll,useTransform,motion,useSpring } from 'framer-motion'
+
 const Slider = () => {
     const {scrollYProgress}=useScroll()
     
