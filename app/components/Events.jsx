bdc0897660a3abb1956dbda00ea63c5f261af16f
Zion livingstone <zionlivingstone4@gmail.com>
2023-09-12 23:02:09 +0100

    x

--- app/components/Events.jsx
+++ app/components/Events.jsx
@@ -133,12 +133,7 @@ const Events = () => {
       }
   }
 
-  var galleria = document.querySelectorAll('.galleria');
-  if (galleria.length > 0) {
-      for (let i = 0; i < galleria.length; i++) {
-        new Galleria(galleria[i]);
-      }
-  }
+  new Galleria(container.current);
 
   },[])
   const svg2=<svg width="164" height="38" viewBox="0 0 164 38" fill="none" xmlns="http://www.w3.org/2000/svg">
